a6e63b6172adab78d90294d654d43eaaf459c94a
Shreyas Dobhal <shreyasdobhal@gmail.com>
2020-06-30 23:05:36 +0530

    App.js file added

---
+++ MERN/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SampleComponent from './components/SampleComponent'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SampleComponent/>
+    </div>
+  );
+}
+
+export default App;
