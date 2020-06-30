9fc07c8d0c2de2d1b5ad0a778a39b5a51fce2eb1
Shreyas Dobhal <shreyasdobhal@gmail.com>
2020-06-30 23:09:58 +0530

    SampleComponent.js file added

---
+++ MERN/client/src/components/SampleComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// const SampleComponent = (props)=>{
+//     return (
+//         <h1>Sample Component</h1>
+//     );
+// }
+
+class SampleComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Sample Component</h1>
+        );
+    }
+}
+
+export default SampleComponent;
