39abe610b38846e6df520e104e868c2be082be05
Shreyas Dobhal <shreyasdobhal@gmail.com>
2020-07-04 16:53:20 +0530

    Page1.js file added

---
+++ ReactJS/src/components/Page1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page1 = ()=>{
+    return (
+        <div className='container'>
+            <h4 className='center'>Page 1</h4>
+        </div>
+    );
+}
+
+export default Page1;
